dfb1a21a8b9276217c7af8569300acae1b631035
caraclarke <cara.clarke8@gmail.com>
2018-01-10 11:24:32 -0500

    add generic first fetch call

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,35 @@
-const test = () => {
-  console.log( "hello" );
+/* **************************
+VARIABLES
+*************************** */
+const root = "https://swapi.co/api/";
+
+/* **************************
+  DOM VARIABLES
+  *************************** */
+
+/* **************************
+  FETCH CALLS
+  *************************** */
+
+/*
+"films": "https://swapi.co/api/films/",
+"people": "https://swapi.co/api/people/",
+"planets": "https://swapi.co/api/planets/",
+"species": "https://swapi.co/api/species/",
+"starships": "https://swapi.co/api/starships/",
+"vehicles": "https://swapi.co/api/vehicles/"
+*/
+
+const fetchAllSections = ( section ) => {
+  fetch( `${ root }/${ section }/`, {
+    method: "get",
+  })
+  .then( function( data ) {
+    console.log( data );
+  })
+  .catch( function( error ) {
+    console.error( error );
+  });
 };
+
+fetchAllSections("people");
